acca22ed818563af005de56106e64f680fecd12a
harakun <dimasoppo448@gmail.com>
2020-11-19 12:54:54 +0700

    read me documentation

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Welcome to CodeIgniter 4!</title>
+    <title>Cat Finder</title>
     <meta name="description" content="The small framework with powerful features">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" type="image/png" href="/favicon.ico" />
